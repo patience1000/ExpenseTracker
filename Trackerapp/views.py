0650e773a25177b9c4d27c681f77e69e3f094907
David D <67209076+hyeladee@users.noreply.github.com>
2024-08-02 21:55:32 +0100

    login fix

--- Trackerapp/views.py
+++ Trackerapp/views.py
@@ -43,6 +43,6 @@ class LoginViewSet(viewsets.ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
-            'username': user.username,
-            'password': user.password
+            'username': user.username,      # No need to return username, but that's not a problem
+            # 'password': user.password     # Returning password (even hashed) is a security issue
         })
