0d35f39ebda15633c61b3a895323cc36e20a6f03
patience1000 <paulpatience577@gmail.com>
2024-08-03 13:04:34 +0100

    modified:   Trackerapp/serializers.py

--- Trackerapp/views.py
+++ Trackerapp/views.py
@@ -40,7 +40,7 @@ class LoginViewSet(viewsets.ViewSet):
         serializer = ObtainAuthToken.serializer_class(data=request.data, context={'request':request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
+        token = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
             'username': user.username,
